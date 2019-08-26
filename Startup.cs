7a0b00c4785af48d6a9887863ac8ab07071ae435
ntello <team@unarix.com.ar>
2019-08-25 23:37:31 -0300

    add: First Template!

--- Startup.cs
+++ Startup.cs
@@ -6,29 +6,63 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace quickland
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            //services.AddMvc();
+            //services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+          
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseCookiePolicy();
+            
+            app.UseFileServer();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+
+            // app.Run(async (context) =>
+            // {
+            //     await context.Response.WriteAsync("Hello World!");
+            // });
+
         }
     }
 }
---
+++ wwwroot/templates/default/implements/default.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: '#qland',
+  data: {
+    qland_titulo: 'Titulo ejemplo',
+    qland_subtitulo: 'Bienvenido!',
+    qland_subtexto: 'Este es un texto de bienvenida que no puede ser muy largo.',
+    qland_notatitulo: 'Que hacemos?',
+    qland_notatexto: 'Nosotros escribimos el texto de la seccion aca... este texto descriptivo puede ser un muy, muy pero muy largo texto; la seccion esta preparada para que puedas insertar bastante texto aqui, asique se creativo y usa muy bien la cabeza.',
+    qland_opcion_uno_titulo: 'Que ta la opción uno?',
+    qland_opcion_uno_texto: 'Este es el texto de la opcion para describir algo. Muchas veces presentar opciones ejemplifican lo que trataste de decir en el parrafo anterior.',
+    qland_opcion_dos_titulo: 'Quizas te guste la opcion dos',
+    qland_opcion_dos_texto: 'Este podria ser el texto que describe la opcion dos, quizas los usuarios quieran elegir o bien pudes usarlo para decorar.',
+    qland_ver_carrousel: true,
+    qland_carrousel_titulo: 'Carrousel',
+    qland_carrousel_texto: 'Este es el texto descriptivo del carrousel'
+  }
+})
